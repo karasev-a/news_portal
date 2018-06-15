3006fb69a432625c77855b55625ca1c08c30ee6e
karasev-a <karasev.avk@gmail.com>
2018-06-15 12:02:59 +0300

    Made routing

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 let http = require("http");
 let fs = require("fs");
 const qs = require("querystring");
-// let User = require("./user.js");
-// let News = require("./news.js");
 let EventEmitter = require("./event_emitter.js");
 let AllNewsUsers = require("./all_news_users.js");
-
 
 let ee = new EventEmitter();
 let allNU = new AllNewsUsers(ee);
 
-let alex  = allNU.createUser("alex");
-let den  = allNU.createUser("den");
-let sam  = allNU.createUser("sam");
-
+let alex = allNU.createUser("alex");
+let den = allNU.createUser("den");
+let sam = allNU.createUser("sam");
 
 allNU.createNews("sport"); //0
 allNU.createNews("it");    //1
 allNU.createNews("fishin");//2
 
+//allNU.subscribeUser(0, alex.id);
 allNU.subscribeUser(0, alex.id);
-allNU.subscribeUser(2, alex.id);
 allNU.subscribeUser(1, den.id);
 allNU.subscribeUser(2, sam.id);
 allNU.subscribeUser(1, sam.id);
@@ -29,7 +25,7 @@ allNU.createNews("sport");
 allNU.createNews("fishin");
 allNU.createNews("it");
 
-allNU.unsubscribeUser(2, alex.id);
+allNU.unsubscribeUser(0, alex.id);
 allNU.unsubscribeUser(1, sam.id);
 
 allNU.createNews("sport");
@@ -41,50 +37,61 @@ console.log(allNU.getNews(0));
 
 
 
+http.createServer((request, response) => {
 
 
+    console.log(request.url);
+    console.log(request.method);
+    let newsId = 0;
+    let userId = 0;
+    let stringArr = request.url.split("/");
+    let reqUrl = request.url;
 
+    if(stringArr[1]=="news"){
+        if(stringArr.length == 5){
+            newsId = Number(stringArr[2]);
+            userId = Number(stringArr[4]);
+        } else if(stringArr.length == 3){
+            newsId = Number(stringArr[2]);
+        }
+    }else if(stringArr[1]=="user"){
+        userId = Number(stringArr[2]);
+    }
 
+    response.write("<html><body><div>");
 
-
-
-
-
-// http.createServer((request, response) => {
-
-//     if (request.method === "GET" && request.url === "/") {
-//         console.log(request.url);
-//         console.log(request.method);
-//         response.end(htmlCash);
-//     } else if (request.method === "POST") {
-//         console.log(request.url);
-//         console.log(request.method);
-
-//         let body = [];
-
-//         request.on('data', (chunk) => {
-//             body.push(chunk);
-//         }).on('end', () => {
-//             body = Buffer.concat(body).toString();
-//             let formData = qs.parse(body.toString());
-
-//             fs.readFile("dataOfForm.json", 'utf8', (err, data) => {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     let oldJsonFileObj = JSON.parse(data);
-//                     oldJsonFileObj.push(formData);
-//                     json = JSON.stringify(oldJsonFileObj);
-//                     fs.writeFile("dataOfForm.json", json, 'utf8', (err) => {
-//                         if (err) {
-//                             throw err;
-//                         } else {
-//                             console.log("The data of form was append to json file");
-//                         }
-//                     });
-//                 }
-//             });
-//         });
-//         response.end(htmlCash);
-//     }
-// }).listen(3000, () => console.log("Server is working"));
+    switch (reqUrl) {
+        case `/news/${newsId}/subscribe/${userId}`:
+            {
+                allNU.subscribeUser(newsId, userId);
+                response.write(`User with id ${userId} subscribes to news with id ${newsId}`);
+            }
+            break;
+        case `/news/${newsId}/unsubscribe/${userId}`:
+            {
+                allNU.unsubscribeUser(newsId, userId);
+                response.write(`User with id ${userId} unsubscribes from news with id ${newsId}`);
+            }
+            break;
+        case `/user/${userId}/subscriptions`:
+            response.write(JSON.stringify(allNU.getSubsriptions(userId), "", 2));
+            break;
+        case `/user/${userId}/export`:
+            {
+                allNU.exportUser(userId);
+                response.write(`Data of user ${userId} write to file`);
+            }
+            break;
+        case `/user/${userId}`:
+            response.write(JSON.stringify(allNU.getUser(userId), "", 2));
+            break;
+        case `/news/${newsId}`:
+            response.write(JSON.stringify(allNU.getNews(newsId), "", 2));
+            break;
+        default:
+            {
+                response.write("</div></body></html>");
+                response.end();
+            }
+    }
+}).listen(3000, () => console.log("Server is working"));
